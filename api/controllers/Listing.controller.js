29b27521caf337372472c13739bd979e8f1c68f2
Mohammad Jawad Hamdan <hamdanjawad789@gmail.com>
2025-06-24 02:53:35 +0300

    created update route

--- api/controllers/Listing.controller.js
+++ api/controllers/Listing.controller.js
@@ -71,3 +71,23 @@ export const deleteListing = async (req, res, next) => {
   }
 
 }
+
+export const updateListing = async (req, res, next) => {
+  const listing=await Listing.findById(req.params.id);
+  if(!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if(req.user.id!== listing.userRef.toString()) {
+    return next(errorHandler(401, "You are not authorized to update this listing"));
+  }
+  try{
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json({ success: true, data: updatedListing });
+  }catch(error){
+    next(error);
+  }
+}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createListing,deleteListing } from "../controllers/Listing.controller.js";
+import { createListing,deleteListing,updateListing } from "../controllers/Listing.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id', verifyToken,deleteListing);
+router.post('/update/:id', verifyToken, updateListing); // Assuming update uses the same controller as create
 
 export default router;
