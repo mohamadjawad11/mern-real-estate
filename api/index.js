2729b763b0f26070ad75da1fffd26060aaa015e3
Mohammad Jawad Hamdan <hamdanjawad789@gmail.com>
2025-08-11 00:04:01 +0300

    bug fix

--- api/index.js
+++ api/index.js
@@ -43,8 +43,10 @@ app.use('/api/listing', getListing);
 app.use('/api/contact', getUser);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
